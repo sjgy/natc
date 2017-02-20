26ee5f47c91cf14770c26b03b5caae6d72982626
cnmz <15510165330@163.com>
2017-02-20 13:59:59 +0800

    error

--- src/router.js
+++ src/router.js
@@ -1,18 +1,24 @@
 import React from 'react'
 import {Router} from 'dva/router'
-import App from './app/router'
 
 export default function({history, app}) {
     const routes = [
         {
-            component: App,
-            getIndexRoute(nextState, cb) {
+            path: '/',
+            getComponent(nextState, cb) {
                 require.ensure([], require => {
-                    cb(null, {component: require('./index/router')})
+                    cb(null, require('./system/index/IndexPage'))
+                })
+            }
+        }, {
+            path: '*',
+            name: 'error',
+            getComponent(nextState, cb) {
+                require.ensure([], require => {
+                    cb(null, require('./system/error/ErrorPage'))
                 })
             }
         }
-    ]
-
+    ];
     return <Router history={history} routes={routes}/>
 }
