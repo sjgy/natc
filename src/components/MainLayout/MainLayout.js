544df61b00b4233aaf9051a31648bf24ca7b7b6e
cnmz <15510165330@163.com>
2017-02-20 18:03:54 +0800

    side

--- src/components/MainLayout/MainLayout.js
+++ src/components/MainLayout/MainLayout.js
@@ -1,19 +1,32 @@
 import React from 'react';
 
 import {Layout, Menu, Icon} from 'antd';
-const {Header, Sider, Content,Footer} = Layout;
+const {Header, Sider, Content, Footer} = Layout;
+
+const SubMenu = Menu.SubMenu;
+const MenuItemGroup = Menu.ItemGroup;
 
 import styles from './MainLayout.less';
 
 const MainLayout = (props) => {
 
-    const {children,status,actions} = props;
+    const {children, status, actions} = props;
 
     return (
         <Layout id="mainLayout">
             <Sider trigger={null} collapsible collapsed={status.collapsed}>
                 <div className="logo"/>
-                <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+                <Menu theme="dark" mode={status.collapsed ? 'vertical' : 'inline'} defaultSelectedKeys={['1']}>
+                  <SubMenu key="sub1" title={< span > <Icon type="mail"/> < span > Navigation One < /span></span >}>
+                      <MenuItemGroup key="g1" title="Item 1">
+                          <Menu.Item key="1">Option 1</Menu.Item>
+                          <Menu.Item key="2">Option 2</Menu.Item>
+                      </MenuItemGroup>
+                      <MenuItemGroup key="g2" title="Item 2">
+                          <Menu.Item key="3">Option 3</Menu.Item>
+                          <Menu.Item key="4">Option 4</Menu.Item>
+                      </MenuItemGroup>
+                  </SubMenu>
                     <Menu.Item key="1">
                         <Icon type="user"/>
                         <span className="nav-text">nav 1</span>
