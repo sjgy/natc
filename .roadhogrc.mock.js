737f0f65d3daa5b8374ba9d6341aeb13d7222d8d
cnmz <15510165330@163.com>
2017-02-21 18:08:37 +0800

    init

--- .roadhogrc.mock.js
+++ .roadhogrc.mock.js
@@ -1,6 +1,8 @@
+import {stringify} from 'qs';
+
 export default {
     'POST /api/users/create' : (req, res) => {
-      console.log(req.body);
-      res.json({"status":"success"});
+        console.log(stringify(req.body));
+        res.json({"status": "success"});
     }
 };
--- src/system/services/app.js
+++ src/system/services/app.js
@@ -1,12 +1,15 @@
 import request from '../utils/request';
 
+import {stringify} from 'qs';
+
 export async function queryUser(params) {
     return request('/api/user', {method: 'get'})
 }
 
-export async function createUser(params) {
+export function createUser(params) {
     return request('/api/users/create', {
-        method: 'post',
-        body: params
+        method: 'POST',
+        headers: new Headers({"Content-Type": "application/x-www-form-urlencoded; charset=utf-8"}),
+        body: stringify(222223)
     });
 }
